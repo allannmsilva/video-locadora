4fa8b920b54b725311a19e6e4fe533f372e22a05
allannmsilva <allannevesmsilva@gmail.com>
2023-09-26 07:16:31 -0300

    Validações de erro

--- src/app/actors/containers/actor-form/actor-form.component.ts
+++ src/app/actors/containers/actor-form/actor-form.component.ts
@@ -1,7 +1,7 @@
 import { Actor } from './../../model/actor';
-import { Location } from '@angular/common';
+import { Location, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -29,14 +29,17 @@ import { ActorsService } from '../../services/actors.service';
     MatToolbarModule,
     MatButtonModule,
     MatSelectModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    NgIf
   ]
 })
 export class ActorFormComponent implements OnInit {
 
   form = this.formBuilder.group({
     _id: [''],
-    name: [''],
+    name: ['', [Validators.required,
+    Validators.minLength(2),
+    Validators.maxLength(100)]]
   });
 
   constructor(private formBuilder: NonNullableFormBuilder,
@@ -70,6 +73,24 @@ export class ActorFormComponent implements OnInit {
   private onSucess() {
     this.openSnackbar('Actor saved successfuly!');
     this.onCancel();
+  }
+
+  onFieldError(fieldName: string) {
+    const field = this.form.get(fieldName);
+    if (field?.hasError('required'))
+      return 'You must enter a value for this field.';
+
+    if (field?.hasError('minlength')) {
+      const requiredLength = field.errors ? field.errors['minlength']['requiredLength'] : 2;
+      return `Insert at least ${requiredLength} characters for this field.`
+    }
+
+    if (field?.hasError('maxlength')) {
+      const requiredLength = field.errors ? field.errors['maxlength']['requiredLength'] : 50;
+      return `Insert at max ${requiredLength} characters for this field.`
+    }
+
+    return 'Invalid field';
   }
 
 }
